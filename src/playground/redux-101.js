034e9929abcadf2d0006d3b33caf55d848f70845
Oliver Lai <oliverlai55@gmail.com>
2018-02-01 15:44:05 -0500

    Play around with redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,53 +4,56 @@ const store = createStore((state = { count: 0 }, action) => {
   // setting default state count:0
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
-        count: state.count
+        count: 0
       };
     default:
       return state;
   }
-
-  // if (action.type === 'INCREMENT') {
-  //   return {
-  //     count: state.count + 1
-  //   };
-  // } else {
-  //   return state;
-  // }
-  console.log('running');
-  return state;
 });
 
-
-console.log(store.getState());
-// get current state
+// calls every single time state changes
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
 
 // Action - an object that gets sent to the store
-// increment, decrement, reset
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+// call once to unsubscribe
+// unsubscribe();
+
 store.dispatch({
   type: 'INCREMENT'
 });
 
 store.dispatch({
-  type: 'DECREMENT'
-});
-
-// RESET - set the count equal to 0
-store.dispatch({
   type: 'RESET'
 });
 
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'DECREMENT',
+  decrementBy: 10
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 101
+});

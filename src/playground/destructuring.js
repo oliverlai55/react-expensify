b7e4aaff3f7bbfc62e5c29101b1619ca73809b21
oliverlai55 <oliverlai55@gmail.com>
2018-02-01 21:03:36 -0500

    Add array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,18 +1,40 @@
-const person = {
-  name: 'Andrew',
-  age: 26,
-  location: {
-    city: 'NYC',
-    temp: 90
-  }
-};
+// const person = {
+// 	name: 'Andrew',
+// 	age: 26,
+// 	location: {
+// 		city: 'NYC',
+// 		temp: 90,
+// 	},
+// };
+//
+// const { name = 'Anonymous', age } = person;
+// // default name value
+// console.log(`${name} is ${age}`);
+//
+// const { city, temp: temperature } = person.location;
+//
+// if (city && temperature) {
+// 	console.log(`It's ${temperature} in ${city}`);
+// }
 
-const { name = 'Anonymous', age } = person;
-// default name value
-console.log(`${name} is ${age}`);
+//
+// Array destructuring
+//
 
-const { city, temp: temperature } = person.location;
+const address = ['1299 S Juniper St', 'New York', 'NY', '10008'];
 
-if (city && temperature) {
-  console.log(`It's ${temperature} in ${city}`);
-}
+const [, city, state = 'CA'] = address;
+// matching up with the position of the array you're pulling from
+// use , if you're not pulling anything
+// you can set default value
+
+console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+// grab first and third items using array destructuring
+console.log(`A medium Coffee (hot) costs $2.50`);
+
+const [itemName, , price] = item;
+
+console.log(`A medium ${itemName} costs ${price}`);

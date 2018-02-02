e7322ab74cfff2bdb042336c4284205b455ac831
oliverlai55 <oliverlai55@gmail.com>
2018-02-02 00:14:53 -0500

    Practice redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,59 +1,65 @@
 import { createStore } from 'redux';
 
+// Action generators - functions
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+	// Destrucutre incrementBy from payload, setting default value as 1 if there is no number
+	// initial state is empty object {} i think?
+	type: 'INCREMENT',
+	incrementBy,
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+	type: 'DECREMENT',
+	decrementBy,
+});
+
+const setCount = ({ setBy = 1 } = {}) => ({
+	type: 'SET',
+	setBy,
+});
+
+const resetCount = () => ({
+	type: 'RESET',
+});
+
 const store = createStore((state = { count: 0 }, action) => {
-  // setting default state count:0
-  switch (action.type) {
-    case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
-      return {
-        count: state.count + incrementBy
-      };
-    case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
-      return {
-        count: state.count - decrementBy
-      };
-    case 'SET':
-      return {
-        count: action.count
-      };
-    case 'RESET':
-      return {
-        count: 0
-      };
-    default:
-      return state;
-  }
+	// setting default state count:0
+	switch (action.type) {
+		case 'INCREMENT':
+			return {
+				count: state.count + action.incrementBy,
+			};
+		case 'DECREMENT':
+			return {
+				count: state.count - action.decrementBy,
+			};
+		case 'SET':
+			return {
+				count: action.setBy,
+			};
+		case 'RESET':
+			return {
+				count: 0,
+			};
+		default:
+			return state;
+	}
 });
 
 // calls every single time state changes
 const unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
+	console.log(store.getState());
 });
 
 // Action - an object that gets sent to the store
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5
-});
+// store.dispatch({
+// 	type: 'INCREMENT',
+// 	incrementBy: 5,
+// });
 
+store.dispatch(incrementCount({ incrementBy: 5 }));
 // call once to unsubscribe
 // unsubscribe();
-
-store.dispatch({
-  type: 'INCREMENT'
-});
-
-store.dispatch({
-  type: 'RESET'
-});
-
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10
-});
-
-store.dispatch({
-  type: 'SET',
-  count: 101
-});
+store.dispatch(decrementCount({ decrementBy: 10 }));
+store.dispatch(resetCount());
+store.dispatch(setCount({ setBy: 100 }));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/playground/destructuring.js',
+	entry: './src/playground/redux-101.js',
 	output: {
 		path: path.join(__dirname, 'public'),
 		filename: 'bundle.js',
@@ -22,7 +22,7 @@ module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	devServer: {
 		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
+		historyApiFallback: true,
 	},
 };
 

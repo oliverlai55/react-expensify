cc42846edced7abd7d675915037d6903214a1170
oliverlai55 <oliverlai55@gmail.com>
2018-02-09 00:35:03 -0500

    Add more test cases

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,8 +1,17 @@
 const add = (a, b) => a + b;
+const generateGreeting = (name = 'Anonymous') => `Hello ${name}!`;
 
 test('should add two numbers', () => {
-  const result = add(3, 4);
-  expect(result).toBe(7);
+	const result = add(3, 4);
+	expect(result).toBe(7);
 });
 
-// 13:18
+test('should generate greeting message', () => {
+	const message = generateGreeting('Oliver');
+	expect(message).toBe('Hello Oliver!');
+});
+
+test('should generate greting for no name', () => {
+	const result = generateGreeting();
+	expect(result).toBe('Hello Anonymous!');
+});

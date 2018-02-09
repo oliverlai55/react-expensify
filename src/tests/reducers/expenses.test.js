f5b60299618b5a96b3aa7e3cb46c7536cdb09844
Oliver Lai <oliverlai55@gmail.com>
2018-02-09 18:04:21 -0500

    Test expense reducer

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -24,8 +24,44 @@ test('should not remove expense if id not found', () => {
   expect(state).toEqual(expenses);
 });
 
-// should add an expense
+test('should add an expense', () => {
+  const expense = {
+    id: '109',
+    description: 'Laptop',
+    note: '',
+    createdAt: 20000,
+    amount: 29500,
+  };
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense,
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
 
-// should edit an expense
+test('should edit an expense', () => {
+  const amount = 122000;
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[1].id,
+    updates: {
+      amount
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[1].amount).toBe(amount);
+});
 
-// should not edit expense if expense not found
+test('should not edit an expense if id not found', () => {
+  const amount = 122000;
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: {
+      amount
+    }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});

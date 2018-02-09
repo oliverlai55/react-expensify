b9cd3a5f805c14e5f2d7eb4c2ae62facc8202717
Oliver Lai <oliverlai55@gmail.com>
2018-02-09 17:54:13 -0500

    Initiate tests for expenses reducer

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,31 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT '});
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[1].id,
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1',
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+// should add an expense
+
+// should edit an expense
+
+// should not edit expense if expense not found

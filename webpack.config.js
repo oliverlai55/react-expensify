f6cc6f9a94d3924875ce2d6339484e8a79ac79a3
oliverlai55 <oliverlai55@gmail.com>
2018-03-03 14:37:41 -0500

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = (env) => {
+module.exports = env => {
 	const isProduction = env === 'production';
 	const CSSExtract = new ExtractTextPlugin('styles.css');
 
 	return {
 		entry: './src/app.js',
 		output: {
-			path: path.join(__dirname, 'public'),
+			path: path.join(__dirname, 'public', 'dist'),
 			filename: 'bundle.js',
 		},
 		module: {
@@ -26,30 +26,28 @@ module.exports = (env) => {
 								loader: 'css-loader',
 								options: {
 									sourceMap: true,
-								}
+								},
 							},
 							{
 								loader: 'sass-loader',
 								options: {
 									sourceMap: true,
-								}
+								},
 							},
-						]
+						],
 					}),
 				},
 			],
 		},
-		plugins: [
-			CSSExtract
-		],
+		plugins: [CSSExtract],
 		devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
 			historyApiFallback: true,
+			publicPath: '/dist/',
 		},
 	};
 };
-
 
 // loader
 // s?css makes s optional

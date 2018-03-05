24ee368cc3ef0cbed2266bb6f0076275473ff6dd
Oliver Lai <oliverlai55@gmail.com>
2018-03-05 18:14:47 -0500

    Add arrays to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,29 +15,64 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
+database.ref('expenses').push({
+	description: 'Rent',
+	note: '',
+	amount: 109500,
+	createdAt: 23453,
+});
+
+database.ref('expenses').push({
+	description: 'Phone Bill',
+	note: '',
+	amount: 5900,
+	createdAt: 23453,
+});
+
+database.ref('expenses').push({
+	description: 'Food',
+	note: '',
+	amount: 1200,
+	createdAt: 234453,
+});
+// database.ref('notes').push({
+// 	title: 'To Do',
+// 	body: 'Go for a run'
+// });
+//
+// database.ref('notes').push({
+// 	title: 'To Do2',
+// 	body: 'Go for a walk'
+// });
+//
+// database.ref('notes/L6sP8ihaBjHibW4-kmZ').update({
+// 	body: 'Buy food',
+// });
+
+// database.ref('notes').set(notes);
 // ref gives us reference to a part of the database
-database
-	.ref()
-	.set({
-		name: 'Oliver Lai',
-		age: 26,
-		stressLevel: 6,
-		job: {
-			title: 'Software Developer',
-			company: 'Google'
-		},
-		isSingle: true,
-		location: {
-			city: 'NYC',
-			country: 'USA',
-		},
-	})
-	.then(() => {
-		console.log('data is saved');
-	})
-	.catch(err => {
-		console.log('this failed', err);
-	});
+// database
+// 	.ref()
+// 	.set({
+// 		name: 'Oliver Lai',
+// 		age: 26,
+// 		stressLevel: 6,
+// 		job: {
+// 			title: 'Software Developer',
+// 			company: 'Google'
+// 		},
+// 		isSingle: true,
+// 		location: {
+// 			city: 'NYC',
+// 			country: 'USA',
+// 		},
+// 	})
+// 	.then(() => {
+// 		console.log('data is saved');
+// 	})
+// 	.catch(err => {
+// 		console.log('this failed', err);
+// 	});
 
 // database.ref()
 // 	.remove()
@@ -101,25 +136,25 @@ database
 // 	});
 
 // This checks for subscription of value changes ('.on')
-const onValueChange = database.ref().on('value', snapshot => {
-	console.log(snapshot.val());
-}, e => {
-	console.log('Error with data fetching', e);
-});
+// const onValueChange = database.ref().on('value', snapshot => {
+// 	console.log(snapshot.val());
+// }, e => {
+// 	console.log('Error with data fetching', e);
+// });
 
-setTimeout(() => {
-	database.ref('age').set(29);
-}, 3500);
+// setTimeout(() => {
+// 	database.ref('age').set(29);
+// }, 3500);
 
-setTimeout(() => {
-	database.ref().off(onValueChange);
-}, 7000);
+// setTimeout(() => {
+// 	database.ref().off(onValueChange);
+// }, 7000);
 
-setTimeout(() => {
-	database.ref('age').set(30);
-}, 10000);
+// setTimeout(() => {
+// 	database.ref('age').set(30);
+// }, 10000);
 
-database.ref().on('value', snapshot) => {
-	const val = snapshot.val();
-	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
-}
+// database.ref().on('value', snapshot) => {
+// 	const val = snapshot.val();
+// 	console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// }

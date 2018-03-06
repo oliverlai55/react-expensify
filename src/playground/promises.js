5ccd73e0ef2953cb764bc6f410d45ca0e7cdb78c
Oliver Lai <oliverlai55@gmail.com>
2018-03-06 15:44:53 -0500

    Refactor test

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -17,6 +17,13 @@ console.log('before');
 promise
 	.then(data => {
 		console.log('2', data);
+
+		return new Promise((resolve, reject) => {
+			setTimeout(() => {
+				resolve('This is my other promise');
+	
+			}, 5000);
+		});
 	})
 	.then(() => {
 		console.log('does this run??');

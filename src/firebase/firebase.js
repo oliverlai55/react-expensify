d7633d94190a1a08014f0e8d91d8ca6166a2af3f
Oliver Lai <oliverlai55@gmail.com>
2018-03-06 13:12:54 -0500

    Update to redux thunk

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -15,26 +15,54 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref('expenses').push({
-	description: 'Rent',
-	note: '',
-	amount: 109500,
-	createdAt: 23453,
-});
+export { firebase, database as default };
 
-database.ref('expenses').push({
-	description: 'Phone Bill',
-	note: '',
-	amount: 5900,
-	createdAt: 23453,
-});
+// database.ref('expenses').on('child_removed', snapshot => {
+// 	console.log(snapshot.key, snapshot.val());
+// });
 
-database.ref('expenses').push({
-	description: 'Food',
-	note: '',
-	amount: 1200,
-	createdAt: 234453,
-});
+// change children
+// database.ref('expenses').on('child_changed', snapshot => {
+// 	console.log(snapshot.key, snapshot.val());
+// });
+
+// child_added, fired every time when child is added.
+// database.ref('expenses').on('child_added', snapshot => {
+// 	console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses')
+// 	.once('value')
+// 	.then(snapshot => {
+// 		const expenses = [];
+// 		snapshot.forEach(childSnapshot => {
+// 			expenses.push({
+// 				id: childSnapshot.key,
+// 				...childSnapshot.val(),
+// 			});
+// 		});
+//
+// 		console.log(expenses);
+// 	});
+
+// database.ref('expenses').on('value', snapshot => {
+// 	const expenses = [];
+// 	snapshot.forEach(childSnapshot => {
+// 		expenses.push({
+// 			id: childSnapshot.key,
+// 			...childSnapshot.val(),
+// 		});
+// 	});
+
+// 	console.log(expenses);
+// });
+// database.ref('expenses').push({
+// 	description: 'Rent',
+// 	note: '',
+// 	amount: 109500,
+// 	createdAt: 23453,
+// });
+
 // database.ref('notes').push({
 // 	title: 'To Do',
 // 	body: 'Go for a run'

a55fa4613f7683bf64ca9379ad2bb7cefe46774b
oliverlai55 <oliverlai55@gmail.com>
2018-04-15 16:40:24 -0400

    Refactor code

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -4,10 +4,10 @@ import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import LoginPage from '../components/LoginPage';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 // we are using custom history npm module instead of BrowserHistory that comes with react router.  We use it so we're able to find history '/' after logging out in app.js
 
@@ -17,11 +17,10 @@ const AppRouter = () => (
 	<Router history={history}>
 		<div>
 			<Switch>
-				<Route path="/" component={LoginPage} exact={true} />
+				<PublicRoute path="/" component={LoginPage} exact={true} />
 				<PrivateRoute path="/dashboard" component={ExpenseDashboardPage} />
 				<PrivateRoute path="/create" component={AddExpensePage} />
 				<PrivateRoute path="/edit/:id" component={EditExpensePage} />
-				<Route path="/help" component={HelpPage} />
 				<Route component={NotFoundPage} />
 			</Switch>
 		</div>

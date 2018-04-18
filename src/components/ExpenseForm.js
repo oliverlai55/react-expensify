f2a1c3fe08b70951c611061d1f110b3f89fdde2e
Oliver Lai <oliverlai55@gmail.com>
2018-04-18 17:07:45 -0400

    Add styling

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,9 +57,8 @@ export default class ExpenseForm extends React.Component {
 	};
 	render() {
 		return (
-			<div>
-				{this.state.error && <p>{this.state.error}</p>}
-				<form onSubmit={this.onSubmit}>
+				<form className="form" onSubmit={this.onSubmit}>
+					{this.state.error && <p className="form__error">{this.state.error}</p>}
 					<input
 						type="text"
 						placeholder="Description"
@@ -89,9 +88,8 @@ export default class ExpenseForm extends React.Component {
 						value={this.state.note}
 						onChange={this.onNoteChange}
 					/>
-					<button>Add Expense</button>
+					<div><button className="button">Add Expense</button></div>
 				</form>
-			</div>
 		);
 	}
 }

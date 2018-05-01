82b0bcea88c5d4085043cd788d79079de4ef61d4
oliverlai55 <oliverlai55@gmail.com>
2018-05-01 01:19:34 -0400

    Change name

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,11 @@ export const Header = ({ startLogout }) => (
 		<div className="content-container">
 			<div className="header__content">
 				<Link className="header__title" to="/dashboard">
-					<h1>Expensify</h1>
+					<h1>StashMaster</h1>
 				</Link>
-				<button className="button button--link" onClick={startLogout}>Logout</button>
+				<button className="button button--link" onClick={startLogout}>
+					Logout
+				</button>
 			</div>
 		</div>
 	</header>

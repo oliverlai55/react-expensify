d953dab0f684ac3a250fdb36c2f44ae3cc007221
Oliver Lai <oliverlai55@gmail.com>
2019-03-19 09:36:58 -0400

    Add progress

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startAddExpense } from '../actions/expenses';
 
-export class AddExpensePage extends React.Component {
+class AddExpensePage extends React.Component {
 	onSubmit = (expense) => {
 		this.props.startAddExpense(expense);
 		this.props.history.push('/');
@@ -32,3 +32,29 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
+
+
+
+// export class AddExpensePage extends React.Component {
+// 	onSubmit = (expense) => {
+// 		this.props.startAddExpense(expense);
+// 		this.props.history.push('/');
+// 		// switch rediret using browser routing without refreshing the page
+// 	};
+// 	render() {
+// 		return (
+// 			<div>
+// 				<div className="page-header">
+// 					<div className='content-container'>
+// 						<h1 className="page-header__title">Add Expense</h1>
+// 					</div>
+// 				</div>
+// 				<div className="content-container">
+// 					<ExpenseForm
+// 						onSubmit = {this.onSubmit}
+// 					/>
+// 				</div>
+// 			</div>
+// 		)
+// 	}
+// }
